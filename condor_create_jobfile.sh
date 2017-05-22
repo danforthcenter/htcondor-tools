a783ba62bd233191614dea0292c5e5978ef8f2b5
calizarr <calizarr@gmail.com>
2017-05-22 16:27:03 -0500

    Updated help and removed some comments

--- condor_create_jobfile.sh
+++ condor_create_jobfile.sh
@@ -3,15 +3,15 @@ usage="$(basename "$0") [-h] [-d DIR -g GLOB -m MAXDEPTH -n NAME -e EXECUTABLE -
 
 where:
     -h		show this help text
-    -d		directory for find
-    -g		glob for find -name option
-    -m		maxdepth for find
+    -d		directory to be passed to find where the directory, file, or files reside in.
+    -g		glob for find -name option: can be using standard glob notation (*) or exact
+    -m		maxdepth for find: 1 for current directory only, 2 for subdirectories
     -n		name for condor job
     -e		executable path for condor job
     -a		arguments for condor job
     -t		number of cpus requested aka threads
-    -l		log directory
-    -s		several logs or just one		
+    -l		log directory where the logs will be written to
+    -s		several logs using the name variable and file variables or just one using the name variable
 
 Example usage:
 1) Before Bash Expansion:
@@ -85,19 +85,19 @@ accounting_group = $ENV(CONDOR_GROUP)
 EOF
 
 # Getting the files and naming them file= and adding a queue afterwards
-# INPUT_1=`find ${FIND_DIR} -maxdepth ${FIND_MAX} -name ${FIND_GLOB} | xargs printf "file=%s\nqueue\n"`
+# This also works for directories just have to make maxdepth be 1 and the glob just be the directory name exact
 INPUT_LOG_SEP=`find ${FIND_DIR} -maxdepth ${FIND_MAX} -name ${FIND_GLOB} | xargs -I {} bash -c 'filename=$(basename {});printf "filename=${filename}\nfile={}\nqueue\n"'`
 
 # Replacing the arguments of the bash script within the INPUT_2 variable
 echo "$INPUT_2" | sed -r "s/(name\s+=\s+)/\1${NAME}/" | sed -r "s/(executable\s+=\s+)/\1${EXECUTABLE}/" | sed -r "s/(arguments\s+=\s+)/\1${ARGUMENTS}/" | sed -r "s/(request_cpus\s+=\s+)/\1${REQUEST_CPUS}/"
-## If the command line paramater -s was included make the log files per file
+## If the command line parameter -s was included make the log files per file instead of constantly appended
 if [ "$SEPARATE" = "true" ]; then
     printf "name\t\t = \$(name).\$(filename)\n"
 else
     INPUT_LOG_SEP=$(echo "$INPUT_LOG_SEP" | sed '/filename/d')
 fi
 
+# Adding in the LOG_DIR variable to the condor job file
 printf "LOG_DIR\t\t = ${LOG_DIR}\n"
 # Printing the files
 printf "%s\n" $INPUT_LOG_SEP
-# printf "%s\n" $INPUT_1
